9afddc2a3ecd127265f9fb207b1274e144271a4f
ArthurMor4is <arthurfmmorais@gmail.com>
2023-12-01 11:25:06 -0300

    init part2 soluiton and reformating script logic

--- 2023/p1/p1.py
+++ 2023/p1/p1.py
@@ -1,4 +1,30 @@
-def main():
+import os
+
+script_directory = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_directory, "input.txt")
+
+numbers_dict = {
+    "one": 1,
+    "1": 1,
+    "two": 2,
+    "2": 2,
+    "three": 3,
+    "3": 3,
+    "four": 4,
+    "4": 4,
+    "five": 5,
+    "5": 5,
+    "six": 6,
+    "6": 6,
+    "seven": 7,
+    "7": 7,
+    "eight": 8,
+    "8": 8,
+    "nine": 9,
+    "9": 9
+}
+
+def part_1():
     total = 0
     with open('input.txt', 'r') as f:
         line = f.readline()
@@ -9,5 +35,31 @@ def main():
             line = f.readline()
     return total
 
+def part_2():
+    total = 0
+    with open(file_path, 'r') as f:
+        line = f.readline()
+        total = 0
+        while line:
+            smallest = float("inf")
+            gratest = -float("inf")
+            first = last = 0
+            for number in numbers_dict.keys():
+                begin = line.find(number)
+                end = line.rfind(number)
+                if begin < smallest and begin != -1:
+                    first = numbers_dict[number]
+                    smallest = begin
+                if end > gratest and end != -1:
+                    last = numbers_dict[number]
+                    gratest = end
+            cal_value = 10*int(first) + int(last)
+            total += cal_value
+            line = f.readline()
+    return total
+
+    
+
 if __name__ == '__main__':
-    print(main())
+    print(part_1())
+    print(part_2())
